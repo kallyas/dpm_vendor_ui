ef8838e909f2d850b2c2fe5baccc45bb91e0cecb
kallyas <kallyasmedia@gmail.com>
2025-12-11 13:27:25 +0300

    Update UserCard.jsx
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

--- src/components/shared/userCard/UserCard.jsx
+++ src/components/shared/userCard/UserCard.jsx
@@ -1,75 +1,58 @@
 import React from 'react';
-import { DirectionsBus, MoreVert, PersonPin, Person, Email, LocalPhone } from '@mui/icons-material';
+import { Paper, Grid, Typography } from '@mui/material';
+import { PersonPin, Person, Email, LocalPhone } from '@mui/icons-material';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
+export const UserCard = (props) => {
+    const {first_name, last_name, email, phone_number} = props;
+
+    return(
+    <Paper sx={{
         width: '300px',
         height: '289px',
         background: '#EEEEEE',
         boxShadow: '4px 5px 6px rgba(162, 48, 47, 0.06)',
         borderRadius: '5px',
         margin: '20px 10px',
-    },
-    top: {
-        width: '100%',
-        height: '138px',
-        background: 'rgba(162, 48, 47, 0.5)',
-        borderRadius: '10px',
-        textAlign: 'center'
-    },
-    bottom: {
-        margin: '10px'
-    },
-    personPin: {
-        width: '114px',
-        height: '120px',
-    },
-    source: {
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        fontWeight: '500',
-        fontSize: '24px',
-        lineHeight: '28px',
-        color: '#865E5D', 
-        padding: '7px 20px 0px 20px'
-        
-    },
-    busTrips: {
-        display: 'inline-flex',
-        color: '#A09D96',
-    },
-    label: {
-        position: "relative",
-        top: "-4px",
-        paddingLeft: '15px',
-        color: 'rgba(162, 48, 47, 0.5)'
-      },
-    record: {
-        marginTop: '10px'
-    }
-}));
-
-export const UserCard = (props) => {
-    const {first_name, last_name, email, phone_number} = props;
-    const classes = useStyles();
-
-    return(
-    <Paper className={classes.root}>
-        <Grid className={classes.top}>
-            <PersonPin className={classes.personPin}/>
+    }}>
+        <Grid sx={{
+            width: '100%',
+            height: '138px',
+            background: 'rgba(162, 48, 47, 0.5)',
+            borderRadius: '10px',
+            textAlign: 'center'
+        }}>
+            <PersonPin sx={{
+                width: '114px',
+                height: '120px',
+            }}/>
         </Grid>
-        <Grid className={classes.bottom}>
-            <Typography className={classes.record}>
+        <Grid sx={{ margin: '10px' }}>
+            <Typography sx={{ marginTop: '10px' }}>
                 <Person />
-                <span className={classes.label}>{`${first_name} ${last_name}`}</span>
+                <span style={{
+                    position: "relative",
+                    top: "-4px",
+                    paddingLeft: '15px',
+                    color: 'rgba(162, 48, 47, 0.5)'
+                }}>{`${first_name} ${last_name}`}</span>
             </Typography>
-            <Typography className={classes.record}>
+            <Typography sx={{ marginTop: '10px' }}>
                 <Email />
-            <span className={classes.label}>{email}</span>
+            <span style={{
+                position: "relative",
+                top: "-4px",
+                paddingLeft: '15px',
+                color: 'rgba(162, 48, 47, 0.5)'
+            }}>{email}</span>
             </Typography>
-            <Typography className={classes.record}>
+            <Typography sx={{ marginTop: '10px' }}>
                 <LocalPhone />
-            <span className={classes.label}>{phone_number}</span>
+            <span style={{
+                position: "relative",
+                top: "-4px",
+                paddingLeft: '15px',
+                color: 'rgba(162, 48, 47, 0.5)'
+            }}>{phone_number}</span>
             </Typography>
         </Grid>
     </Paper>
