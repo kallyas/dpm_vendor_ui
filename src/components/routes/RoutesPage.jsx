850d0d8e8479c0f76da666a50f9d6e3cd79a1f4e
kallyas <kallyasmedia@gmail.com>
2026-02-28 19:57:11 +0300

    refactor: Migrate RoutesPage to functional component with RTK Query
    The Routes class component was refactored into a functional RoutesPage component.
    Data fetching was migrated from Redux `connect`, `getRoutes` thunk, and lifecycle methods (`componentDidMount`, `componentWillReceiveProps`) to the `useGetRoutesQuery` hook from RTK Query.
    
    This change simplifies component logic and state management by leveraging RTK Query's declarative data fetching, caching, and loading states.
    Removed: `connect`, `mapStateToProps`, `mapDispatchToProps`, `Component`, `toast`.
    Introduced: `useState`, `useGetRoutesQuery`, and direct destructuring of `data`, `isLoading`, `error` from the hook.
    The `handleCheck` function was simplified, and `allChecked` is now hardcoded to `false`, as selection state is no longer managed locally.
    
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

--- src/components/routes/RoutesPage.jsx
+++ src/components/routes/RoutesPage.jsx
@@ -1,104 +1,57 @@
-import React, {Component} from "react"
-import {Container, Box, Typography} from "@mui/material"
-import SharedTable from "../shared/sharedTable/SharedTable"
-import {getRoutes} from "../../redux/slices/routesSlice"
-import {connect} from "react-redux"
-import {toast} from "react-toastify"
+import { useState } from "react";
+import { Container, Box, Typography } from "@mui/material";
+import SharedTable from "../shared/sharedTable/SharedTable";
+import { useGetRoutesQuery } from "../../redux/api/apiSlice";
 
-class Routes extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      routes: [],
-      fetching: "129.5%",
-      headers: [
-        {label: "Route Code", key: "route_code"},
-        {label: "From", key: "start_point"},
-        {label: "To", key: "destination"},
-      ],
-    }
-  }
+const RoutesPage = () => {
+  const { data: routes = [], isLoading, error } = useGetRoutesQuery();
 
-  componentDidMount() {
-    const {getRoutes} = this.props
-    getRoutes()
-  }
+  const headers = [
+    { label: "Route Code", key: "route_code" },
+    { label: "From", key: "start_point" },
+    { label: "To", key: "destination" },
+  ];
 
-  componentWillReceiveProps(nextProps) {
-    const {
-      routes: {data, error},
-    } = nextProps
+  const handleCheck = ({ target }) => {
+    const { checked, id } = target;
+  };
 
-    if (error) {
-      this.setState({fetching: false})
-      toast.error(error.message)
-    } else {
-      this.setState({fetching: false, routes: data})
-    }
-  }
+  const allChecked = false;
+  const fetching = isLoading;
 
-  handleCheck = ({target}) => {
-    const {checked, id} = target
-    const {routes} = this.state
-    const checkedRoutes = routes.map((route) => {
-      const checkedRoute = {...route}
-      if (id === "header") {
-        checkedRoute.checked = checked
-      }
-      if (route.id == id) {
-        checkedRoute.checked = checked
-      }
-      return checkedRoute
-    })
+  return (
+    <Container maxWidth="lg" sx={{ py: 4, px: { xs: 2, sm: 3, md: 4 } }}>
+      <Box sx={{ mb: 4 }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            fontWeight: 700,
+            color: "#A2302F",
+            mb: 1,
+          }}
+        >
+          Routes Management
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "text.secondary",
+          }}
+        >
+          View and manage all routes in your system.
+        </Typography>
+      </Box>
+      <SharedTable
+        fetching={fetching}
+        hideDock={true}
+        handleCheck={handleCheck}
+        tableBody={routes}
+        headers={headers}
+        allChecked={allChecked}
+      />
+    </Container>
+  );
+};
 
-    this.setState({routes: checkedRoutes})
-  }
-
-  render() {
-    const {headers, routes, fetching} = this.state
-    const allChecked = routes && routes.length > 0 && routes.every((route) => route.checked == true)
-    return (
-      <Container maxWidth="lg" sx={{ py: 4, px: {xs: 2, sm: 3, md: 4} }}>
-        <Box sx={{ mb: 4 }}>
-          <Typography 
-            variant="h4" 
-            component="h1"
-            sx={{
-              fontWeight: 700,
-              color: "#A2302F",
-              mb: 1,
-            }}
-          >
-            Routes Management
-          </Typography>
-          <Typography 
-            variant="body1" 
-            sx={{
-              color: "text.secondary",
-            }}
-          >
-            View and manage all routes in your system.
-          </Typography>
-        </Box>
-        <SharedTable
-          fetching={fetching}
-          hideDock={true}
-          handleCheck={this.handleCheck}
-          tableBody={routes}
-          headers={headers}
-          allChecked={allChecked}
-        />
-      </Container>
-    )
-  }
-}
-
-const mapStateToProps = ({routes}) => {
-  return {routes}
-}
-
-const mapDispatchToProps = {
-  getRoutes,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Routes)
+export default RoutesPage;
